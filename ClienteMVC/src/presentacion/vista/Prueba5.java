b85e1d092e36e840506c4e64563249ecb562ecab
Jose\Jose <jose.orozco229141@potros.itson.edu.mx>
2022-12-01 12:57:09 -0700

    compilado 2.0

---
+++ ClienteMVC/src/presentacion/vista/Prueba5.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package presentacion.vista;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author FamSc
+ */
+public class Prueba5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int[] arr = {10,25,23,14,85,65};
+            int key = 14;
+        System.out.println( );
+    }
+    
+}
