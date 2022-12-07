6ef3b5eee12c851ca74464f24d99beb3ca33c257
Jose\Jose <jose.orozco229141@potros.itson.edu.mx>
2022-12-07 15:27:35 -0700

    as

--- ClienteMVC/src/presentacion/vista/FJuego.java
+++ ClienteMVC/src/presentacion/vista/FJuego.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Observable;
 import java.util.Random;
+import javax.swing.JOptionPane;
 import modelo.MPartida;
 import presentacion.dibujo.DadoGrafico;
 import presentacion.dibujo.TableroGrafico;
@@ -105,7 +106,8 @@ public class FJuego extends FrameBase {
         this.dadoJ.setDados(partida.getDados());
                 }
         else{
-            System.out.println("La partida ha finalizado; Ganador="+partida.getTurno().getNombre() );
+            JOptionPane.showMessageDialog(null, "La partida ha finalizado; Ganador="+partida.getTurno().getNombre() );
+                        System.exit(0);
         }
     }
 
